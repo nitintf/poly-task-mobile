cdf4dba8cfc1305be8da26ca928339bd89b1110d
Nitin Panwar <nitin.panwar@bitcs.in>
2023-10-23 19:57:58 +0530

    init app

---
+++ app/navigators/AppNavigator.tsx
@@ -0,0 +1,67 @@
+import { DarkTheme, DefaultTheme, NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator, NativeStackScreenProps } from "@react-navigation/native-stack"
+import { observer } from "mobx-react-lite"
+import React from "react"
+import { useColorScheme } from "react-native"
+import * as Screens from "app/screens"
+import Config from "../config"
+import { useStores } from "../models"
+import { navigationRef, useBackButtonHandler } from "./navigationUtilities"
+import { colors } from "app/theme"
+
+export type AppStackParamList = {
+  Home: undefined
+}
+
+const exitRoutes = Config.exitRoutes
+
+export type AppStackScreenProps<T extends keyof AppStackParamList> = NativeStackScreenProps<
+  AppStackParamList,
+  T
+>
+
+// Documentation: https://reactnavigation.org/docs/stack-navigator/
+const Stack = createNativeStackNavigator<AppStackParamList>()
+
+const AppStack = observer(function AppStack() {
+  const {
+    authenticationStore: { isAuthenticated },
+  } = useStores()
+
+  return (
+    <Stack.Navigator
+      screenOptions={{ headerShown: false, navigationBarColor: colors.background }}
+      initialRouteName={"Login"}
+    >
+      {/* {isAuthenticated ? (
+        <>
+          <Stack.Screen name="Welcome" component={Screens.WelcomeScreen} />
+          <Stack.Screen name="Demo" component={DemoNavigator} />
+        </>
+      ) : (
+        <>
+        </>
+      )} */}
+      <Stack.Screen name="" component={Screens.LoginScreen} />
+    </Stack.Navigator>
+  )
+})
+
+export interface NavigationProps
+  extends Partial<React.ComponentProps<typeof NavigationContainer>> {}
+
+export const AppNavigator = observer(function AppNavigator(props: NavigationProps) {
+  const colorScheme = useColorScheme()
+
+  useBackButtonHandler((routeName) => exitRoutes.includes(routeName))
+
+  return (
+    <NavigationContainer
+      ref={navigationRef}
+      theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+      {...props}
+    >
+      <AppStack />
+    </NavigationContainer>
+  )
+})
